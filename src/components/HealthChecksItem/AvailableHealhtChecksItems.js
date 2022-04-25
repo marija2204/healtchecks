508fc40b9b32efbec10fe871568ee4fd5c3e5761
marija2204 <marijaudovicic97@gmail.com>
2022-04-25 10:50:13 +0200

    http

--- src/components/HealthChecksItem/AvailableHealhtChecksItems.js
+++ src/components/HealthChecksItem/AvailableHealhtChecksItems.js
@@ -6,11 +6,20 @@ import './AvailableHealthChecks.css'
 const AvailableHealthChecksItems = () => {
    
     const [items, setItems] = useState([]);
+    const [HttpError, setHttpError] = useState();
+    const [isLoading, setIsLoading] = useState(true);
 
    useEffect(() => {
 
    fetch('https://services.testnisite.com:8103/background/sport/health/ready').then(response => {
+      if(!response.ok){
+        
+         if(response.status ==! 503){
+            throw new Error(response.statusText);
+          }
+      }
       return response.json()
+
    }).then(data =>{
        let keys = Object.keys(data.DependencyHealthChecks);
        let values = Object.values(data.DependencyHealthChecks);
@@ -20,13 +29,24 @@ const AvailableHealthChecksItems = () => {
        }
      
        setItems(values);
-
+       setIsLoading(false);
+     
+      }).catch((error) => {
+           setHttpError(error.message);
+           setIsLoading(false);
       });
     },[])
+
+    if(isLoading) {
+      return <section className='DatasLoading'>
+        <p>Loading...</p>
+      </section>
+    }
 
     const list = items.length > 0 ? items.map((item) => {
         
       return ( <HealthChecksItem
+         key={item.name}
          name={item.name}
          status={item.Status}
          description={item.Description}
@@ -34,12 +54,10 @@ const AvailableHealthChecksItems = () => {
         />)
 
         }) : null;
-
-      console.log(list);
     return (
      <section>
-         <div>
-         {list && list.length > 0 && 
+         <div className="error">
+         {HttpError ? HttpError : list && list.length > 0 && 
         <ul className="items-list">{list}</ul>
          }
         </div>
--- src/components/HealthChecksOverall/HealthChecksOverall.js
+++ src/components/HealthChecksOverall/HealthChecksOverall.js
@@ -1,27 +1,53 @@
 
-import {useState} from "react";
+import {useState,useEffect} from "react";
 import './HealthChecksOverall.css'
 import HealthChecksIcon from "../HealthChecksItem/HealthChecksIcon";
 
 const HealthChecksOverall = () => {
   const [status, setStatus]= useState();
   const [duration, setDuration] = useState();
-  
-fetch('https://services.testnisite.com:8103/background/sport/health/ready').then(response => {
-  return response.json();
-}).then((data) => {
-  setStatus(data.OverallStatus);
-  setDuration(data.TotalChecksDuration)
-});
+  const [httpError, setHttpError] = useState();
+  const [isLoading, setIsLoading] = useState(true);
 
-  return(
-           <div className="example">
-              <div>Overall Status</div>
-              <div><HealthChecksIcon status={status}/>{status}</div>
-              <div>{duration}</div>
-          </div>
-       
+ useEffect(() => {
+  fetch('https://services.testnisite.com:8103/background/sport/health/ready').then(response => {  
+      if(!response.ok){
+        
+        if(response.status ==! 503){
+          throw new Error(response.statusText);
+        }
+      }
+      return response.json();
+
+    }).then((data) => {
+
+      setStatus(data.OverallStatus);
+      setDuration(data.TotalChecksDuration);
+      setIsLoading(false);
+
+    }).catch((error) => {
+           setHttpError(error.message);
+           setIsLoading(false);
+     });
+ 
+ }, []);
+ 
+ if(isLoading) {
+  return <section className='DatasLoading'>
+    <p>Loading...</p>
+  </section>
+  };
+
+
+  return(<div className="error">
+           {httpError ? httpError : (
+                      <div className="example">
+                        <div>Overall Status</div>
+                        <div><HealthChecksIcon status={status}/>{status}</div>
+                        <div>{duration}</div>
+                      </div>
+       )}</div>
   );
-};
+  };
 
 export default HealthChecksOverall;
