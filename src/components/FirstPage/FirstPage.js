55e0043661e52f8919d710ca0f19036d4e263e27
marija2204 <marijaudovicic97@gmail.com>
2022-05-04 15:51:10 +0200

    izmjene

--- src/components/FirstPage/FirstPage.js
+++ src/components/FirstPage/FirstPage.js
@@ -1,9 +1,6 @@
 import React from "react";
-import classes from './FirstPage.module.css';
-import { useDispatch, useSelector } from "react-redux";
-import { selectActions } from '../../store/select';
+import './FirstPage.css';
 import { Link } from "react-router-dom";
-import Header from "../Layout/Header";
 import FirstPageTitle from "./FirstPageTitle";
 
 const FirstPage = () => {
@@ -17,12 +14,12 @@ const FirstPage = () => {
     return (
       <div>
        <FirstPageTitle/>
-        <main className={classes.products}>
+        <main className='products'>
           <h1>My Products</h1>
           <div>
-             <button><Link to="/test1" className={classes.link}>TEST 1</Link></button>
-             <button> <Link to="/test2" className={classes.link}>TEST 2</Link></button>
-             <button><Link to="/test3" className={classes.link}>TEST 3</Link></button>
+             <Link  className='btn' to="/test1" >TEST 1</Link>
+             <Link  className='btn' to="/test2" >TEST 2</Link>
+             <Link  className='btn' to="/test3" >TEST 3</Link>
           </div>
         </main>
         </div>
